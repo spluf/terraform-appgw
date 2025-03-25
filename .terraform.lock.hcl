f276221219ccf331c97ca3ed8f0de12f3f08227d
spluf <Cosmin.s.Pop@gmail.com>
2025-03-25 12:09:23 +0200

    added comments

---
+++ .terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azuread" {
+  version = "3.2.0"
+  hashes = [
+    "h1:DciAAiQWGJK0FR+2Qe/O/NwukMs6nLtVqv19BQY6hU4=",
+    "zh:063897b38fd6231577c21a751ccd955716bca0e3abcf800a90dd1dd50fb24875",
+    "zh:19704c38c0cf229dc663e36a3ea53649780b7b5ec3953febbc95d7cb5e1cde27",
+    "zh:1c3e89cf19118fc07d7b04257251fc9897e722c16e0a0df7b07fcd261f8c12e7",
+    "zh:463c9351c86d161b6c81efd3109d45acec45f4269bf0180a589073eb5ab6b3e1",
+    "zh:54a1962e9f998f3725d801274fb8e0a1a6ad194e50f599d586aca265ed193934",
+    "zh:64fafb076204728168a3c369b9412a0c58ee44ac3175d79611626725e812e73a",
+    "zh:7283c00ca13c7e1a5d2976884a314daed6dea7e5e99e33d8f99eda28b277c12a",
+    "zh:95a564a55d2ed8cac1299577f7b1671e0f9d13c0429f5e321deabadb0740efed",
+    "zh:a325b5c5ea1a8188ad51c4d3ffea76362895bc4749c613e2ec5ac807b3ede902",
+    "zh:b3d58bb47fdb747734fdefef8bb7d08ae45fd8634792d839043ae326f333b33e",
+    "zh:da1b6043e5615f767b9685ab4cfdffc04e1b5ca808c7c004d83bac75cbc8f465",
+    "zh:fd5eb49039b7787a1133cbb9f2ba6b0183de10e7ba95aaaaebdee7b50e40c6d9",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "4.1.0"
+  constraints = "4.1.0"
+  hashes = [
+    "h1:cwtEEnEESVOgcxtXGz0A2wXCiNZIzm3dC2xHmYuxg9M=",
+    "zh:3f332bca3a8b7dc982e428e09c73862d1afda34c2ad7803e70d8ba7b9e2445fd",
+    "zh:66b7e4a7a4fd06e0a5a3a22b4f76bda48e50ed3dd26c388738d9cc882b801bce",
+    "zh:6a271175d6e079241f24129f5026e0b16f04e7a548807f115600003d615e0ec3",
+    "zh:7a6abc7e2ae8d1041d0446bbe87156e0436639676ee1fad40321e8ee6759a454",
+    "zh:903f6e7f03e5952347ce6ee589d58c829179f2f22220f25cf52ae4efecd7053c",
+    "zh:a572b9834cf3b51799c82c5009705c59309d947a6ecdb7e17729868c55e7d0e0",
+    "zh:a7fca14338f0cfb82b17ce085400c210cbc986a87086702e3a11efcb4e53d6e4",
+    "zh:af36c7004702b0a273794914a17a77af1eb972caaad64e0068739e55c1488845",
+    "zh:b36f308db1cdc02dee659e3e518186d7dec970d88b6149be3f6b3f8d544e4282",
+    "zh:bedf6d13cf4bccc128d8cbb0703a3a8b547629674439ffda5e73563ab775d0f7",
+    "zh:d1df286a2e5d4a5f6a7f4d29700a25588167ccffa31c686550ef617503df3254",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
--- appgw.tf
+++ appgw.tf
@@ -2,20 +2,21 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "=4.1.0" // Specify the AzureRM provider version
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  features {} // Enable all features for the AzureRM provider
 
-  subscription_id = "00000000-00000-00000-0000000000"
+  subscription_id = "00000000-0000-0000-0000000000" // Azure subscription ID
 }
 
 # since these variables are re-used - a locals block makes this more maintainable
 locals {
+  // Define reusable variables for resource names and configurations
   backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
   frontend_port_name_5000        = "${azurerm_virtual_network.example.name}-feport5000"
   frontend_port_name_5001        = "${azurerm_virtual_network.example.name}-feport5001"
@@ -26,25 +27,30 @@ locals {
   request_routing_rule_name_5000 = "${azurerm_virtual_network.example.name}-rqrt5000"
   request_routing_rule_name_5001 = "${azurerm_virtual_network.example.name}-rqrt5001"
   redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
-  resource_group_name            = "example-resources"
+  resource_group_name            = "cosmin-rg"
   location                       = "Poland Central"
 }
 
-data "azurerm_client_config" "current" { }
+data "azurerm_client_config" "current" {
+  // Fetch details of the current Azure client configuration
+}
 
 data "azuread_user" "current_user" {
+  // Retrieve details of the current Azure Active Directory user
   object_id = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_resource_group" "example" {
+  // Create a resource group
   name     = local.resource_group_name
   location = local.location
   tags = {
-    userCreated = data.azuread_user.current_user.user_principal_name
+    userCreated = data.azuread_user.current_user.user_principal_name // Tag with the creator's username
   }
 }
 
 resource "azurerm_role_assignment" "example" {
+  // Assign the "Owner" role to the current user for the resource group
   depends_on = [ azurerm_resource_group.example ]
   scope                = azurerm_resource_group.example.id
   role_definition_name = "Owner"
@@ -52,6 +58,7 @@ resource "azurerm_role_assignment" "example" {
 }
 
 resource "azurerm_user_assigned_identity" "example" {
+  // Create a User Assigned Identity
   depends_on = [ azurerm_resource_group.example ]
   location            = local.location
   name                = "example-identity"
@@ -59,6 +66,7 @@ resource "azurerm_user_assigned_identity" "example" {
 }
 
 resource "azurerm_role_assignment" "example2" {
+  // Assign the "Owner" role to the User Assigned Identity for the resource group
   depends_on = [ azurerm_user_assigned_identity.example ]
   scope                = azurerm_resource_group.example.id
   role_definition_name = "Owner"
@@ -66,66 +74,76 @@ resource "azurerm_role_assignment" "example2" {
 }
 
 resource "azurerm_virtual_network" "example" {
+  // Create a virtual network
   depends_on = [ azurerm_resource_group.example ]
   name                = "example-network"
   resource_group_name = local.resource_group_name
   location            = local.location
-  address_space       = ["10.254.0.0/16"]
+  address_space       = ["10.254.0.0/16"] // Define the address space
 }
 
 resource "azurerm_public_ip" "example" {
+  // Create a public IP address
   depends_on = [ azurerm_resource_group.example ]
   name                = "example-pip"
   resource_group_name = local.resource_group_name
   location            = local.location
-  allocation_method   = "Static"
+  allocation_method   = "Static" // Use a static IP
 }
 
 resource "azurerm_subnet" "example" {
+  // Create a subnet within the virtual network
   depends_on = [ azurerm_virtual_network.example ]
   name                 = "example-subnet"
   resource_group_name  = local.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.254.0.0/24"]
+  address_prefixes     = ["10.254.0.0/24"] // Define the subnet address range
 }
 
 resource "azurerm_application_gateway" "example" {
+  // Create an Application Gateway
   depends_on = [ azurerm_public_ip.example, azurerm_subnet.example, azurerm_user_assigned_identity.example, azurerm_role_assignment.example ]
   name                = "example-appgateway"
   resource_group_name = local.resource_group_name
   location            = local.location
 
   sku {
-    name     = "Standard_v2"
+    name     = "Standard_v2" // Specify the SKU
     tier     = "Standard_v2"
-    capacity = 2
+    capacity = 2 // Number of instances
   }
 
   gateway_ip_configuration {
+    // Configure the gateway's IP settings
     name      = "example-gateway-ip-configuration"
     subnet_id = azurerm_subnet.example.id
   }
 
   frontend_port {
+    // Define a frontend port for HTTP traffic on port 5000
     name = local.frontend_port_name_5000
     port = 5000
   }
 
   frontend_port {
+    // Define a frontend port for HTTP traffic on port 5001
     name = local.frontend_port_name_5001
     port = 5001
   }
 
   frontend_ip_configuration {
+    // Configure the frontend IP with the public IP address
     name                 = local.frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.example.id
   }
 
   backend_address_pool {
+    // Define a backend address pool
     name = local.backend_address_pool_name
   }
 
   backend_http_settings {
+    // Configure backend HTTP settings
     name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
     path                  = "/path1/"
@@ -135,13 +153,15 @@ resource "azurerm_application_gateway" "example" {
   }
 
   http_listener {
+    // Create an HTTP listener for port 5000
     name                           = local.listener_name_5000
     frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = local.frontend_port_name_5000
     protocol                       = "Http"
   }
 
-    http_listener {
+  http_listener {
+    // Create an HTTP listener for port 5001
     name                           = local.listener_name_5001
     frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = local.frontend_port_name_5001
@@ -149,6 +169,7 @@ resource "azurerm_application_gateway" "example" {
   }
 
   request_routing_rule {
+    // Define a routing rule for port 5000
     name                       = local.request_routing_rule_name_5000
     priority                   = 9
     rule_type                  = "Basic"
@@ -158,6 +179,7 @@ resource "azurerm_application_gateway" "example" {
   }
 
   request_routing_rule {
+    // Define a routing rule for port 5001
     name                       = local.request_routing_rule_name_5001
     priority                   = 10
     rule_type                  = "Basic"
@@ -167,12 +189,14 @@ resource "azurerm_application_gateway" "example" {
   }
 
   identity {
+    // Attach a User Assigned Identity to the Application Gateway
     type = "UserAssigned"
     identity_ids = [ azurerm_user_assigned_identity.example.id ]
   }
 }
 
 resource "azurerm_role_assignment" "example3" {
+  // Assign the "Owner" role to the User Assigned Identity for the Application Gateway
   depends_on = [ azurerm_application_gateway.example ]
   scope                = azurerm_application_gateway.example.id
   role_definition_name = "Owner"
@@ -180,6 +204,7 @@ resource "azurerm_role_assignment" "example3" {
 }
 
 resource "azurerm_role_assignment" "example4" {
+  // Assign the "Network Contributor" role to the User Assigned Identity for the Virtual Network
   depends_on = [ azurerm_application_gateway.example ]
   scope                = azurerm_virtual_network.example.id
   role_definition_name = "Network Contributor"
